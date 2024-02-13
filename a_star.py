6b2e4a93f2408f1e71d0306dbbb69b8d353d50c4
Desk1 <desk1@pop-os.localdomain>
2024-02-12 20:01:12 -0600

    Initial Commit

---
+++ a_star.py
@@ -0,0 +1,68 @@
+from typing import List, Tuple, Dict, Callable, Set
+from copy import deepcopy
+from math import atan2
+
+def apply_move(position:Tuple[int, int], move:Tuple[int, int]) -> float:
+    return (position[0]+move[0],position[1]+move[1])
+
+
+def heuristic(position:Tuple[int, int], goal:Tuple[int, int], costs: Dict[str, int], moves: List[Tuple[int, int]]) -> float:
+    movement_angles = [atan2(move[1],move[0]) for move in moves]
+    vector_to_goal = (goal[0]-position[0],goal[1]-position[1])
+    angle_to_goal = atan2(vector_to_goal[1],vector_to_goal[0])
+    angle_offs = [abs(angle-angle_to_goal) for angle in movement_angles]
+    chosen_direction = moves[angle_offs.index(min(angle_offs))]
+
+    num_moves = 0
+    while not atan2(vector_to_goal[1],vector_to_goal[0]) in movement_angles:
+        vector_to_goal = (vector_to_goal[0]-chosen_direction[0],vector_to_goal[1]-chosen_direction[1])
+        num_moves += 1
+    
+    angle_to_goal = atan2(vector_to_goal[1],vector_to_goal[0])
+    angle_offs = [abs(angle-angle_to_goal) for angle in movement_angles]
+    chosen_direction = moves[angle_offs.index(min(angle_offs))]
+    num_moves += (chosen_direction[0]*vector_to_goal[0]+chosen_direction[1]*vector_to_goal[1])/((chosen_direction[0])**2+(chosen_direction[1])**2)
+
+    return num_moves * min(costs.values())
+
+def extract_moves_from_path(explored_list: List[Dict]) -> List[Tuple[int, int]]:
+    moves = []
+    current_state = explored_list[-1]
+    while current_state['parent'] != -1:
+        prev_state = explored_list[current_state['parent']]
+        moves.append(((current_state['state'][0]-prev_state['state'][0]),(current_state['state'][1]-prev_state['state'][1])))
+        current_state = prev_state
+    return moves[::-1]
+
+def a_star_search( world: List[List[str]], start: Tuple[int, int], goal: Tuple[int, int], costs: Dict[str, int], moves: List[Tuple[int, int]], heuristic: Callable) -> List[Tuple[int, int]]:
+    explored_list = []
+    parent_index = -1
+    frontier_queue = [{'state':start, 'cost':0, 'parent':parent_index}]
+    untouched_states = set()
+    [[untouched_states.add((c,r)) for c,col in enumerate(row) if (c,r) != start] for r,row in enumerate(world)]
+    while parent_index == -1 or explored_list[-1]['state'] != goal:
+        parent_index += 1
+        current_state = frontier_queue.pop()
+        sucessors = [x for x in [apply_move(current_state['state'],move) for move in moves] if x in untouched_states]
+        [untouched_states.discard(state) for state in sucessors]
+        sucessors = [state for state in sucessors if world[state[1]][state[0]] in costs.keys()]
+        frontier_queue.extend([{
+            'state':state, 
+            'cost':current_state['cost']+costs[world[state[1]][state[0]]] + heuristic(current_state['state'], goal, costs, moves),
+            'parent':parent_index
+            } for state in sucessors])
+        frontier_queue.sort(key= lambda x: -x['cost'])
+        explored_list.append(current_state)
+    return extract_moves_from_path(explored_list)
+
+def pretty_print_path( world: List[List[str]], path: List[Tuple[int, int]], start: Tuple[int, int], goal: Tuple[int, int], costs: Dict[str, int], moves,  move_icons) -> int:
+    path_map = deepcopy(world)
+    position = start
+    cost = 0
+    for move in path:
+        path_map[position[1]][position[0]] = move_icons[moves.index(move)]
+        position = apply_move(position,move)
+        cost += costs[world[position[1]][position[0]]]
+    path_map[goal[0]][goal[1]] = '🎁'
+    print('\n'.join([''.join(row) for row in path_map]))
+    return cost # replace with the real value!
