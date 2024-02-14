82a35074c0b7b39bf98313ed2c1b066f994d4428
Desk1 <desk1@pop-os.localdomain>
2024-02-13 20:07:39 -0600

    S

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,11 @@
 import streamlit as st
 from a_star import *
-print(full_world)
-
-
+widget_id = (id for id in range(1, 100_00))
 
 cols = st.columns([1]*len(full_world[0]))
 
 for c,col in enumerate(cols):
     with col:
         for r,element in enumerate(full_world):
-            if st.button(full_world[r][c]):
+            if st.button(full_world[r][c], key=next(widget_id)):
                 st.write(str(r)+str(c))
