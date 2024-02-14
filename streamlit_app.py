5511057a28d9f4f1c99380b3f3de5c8cd300e952
Desk1 <desk1@pop-os.localdomain>
2024-02-13 20:01:49 -0600

    S

---
+++ streamlit_app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+from a_star import *
+print(full_world)
+
+
+
+cols = st.columns([1]*len(full_world[0]))
+
+for c,col in enumerate(cols):
+    with col:
+        for r,element in full_world:
+            st.button(full_world[r][c])
