a9b0981b8a8a3aa778b08251ad7e6189418ab470
Desk1 <desk1@pop-os.localdomain>
2024-02-13 20:18:38 -0600

    S

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ widget_id = (id for id in range(1, 100_00))
 
 full_world_transposed = [[row[i] for row in full_world] for i in range(len(full_world[0]))]
 
-cols = st.columns([1/len(full_world_transposed[0])]*len(full_world_transposed[0]),gap='large')
+cols = st.columns([1/len(full_world_transposed[0])]*len(full_world_transposed[0]),gap='medium')
 
 for c,col in enumerate(full_world_transposed):
     with cols[c]:
