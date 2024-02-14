9b651bcb9a0105f33114a36a807be33fe5260d31
Desk1 <desk1@pop-os.localdomain>
2024-02-13 21:34:56 -0600

    S

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,12 +11,13 @@ cols = st.columns(len(full_world_transposed[0]),gap='small')
 if 'buttons_clicked' not in st.session_state:
     st.session_state.buttons_clicked = []
 
-def click_button(button_key):
+def click_button(container, button_key):
     if len(st.session_state.buttons_clicked) <= 1:
-        st.session_state.buttons_clicked.append(button_key)
-        st.session_state[button_key].label = 'h'
+        st.session_state.buttons_clicked.append(container)
+        container.empty()
+        container.button(full_world_transposed[c][r], key=button_key, on_click=click_button, args=(container,button_key))
     else:
-        st.session_state.buttons_clicked = [button_key]
+        st.session_state.buttons_clicked = [container]
 
 # help(st.button)
 
@@ -24,6 +25,6 @@ for c,col in enumerate(full_world_transposed):
     with cols[c]:
         for r,element in enumerate(col):
             button_key = str(r)+'.'+str(c)
-            print(button_key)
-            if st.button(full_world_transposed[c][r], key=button_key, on_click=click_button, args=(button_key,)):
+            container = st.container()
+            if container.button(full_world_transposed[c][r], key=button_key, on_click=click_button, args=(container,button_key)):
                 print('1')
