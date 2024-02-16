8aa78087f90f70391f3c6c69472016ef808d9c47
Desk1 <desk1@pop-os.localdomain>
2024-02-15 19:44:38 -0600

    S

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,11 +21,11 @@ def click_button(r,c):
     if len(st.session_state.buttons_clicked) == 0:
         st.session_state.buttons_clicked.append((r,c))
     if len(st.session_state.buttons_clicked) == 1:
-        st.session_state.buttons_clicked.append((r,c))
+        st.session_state.buttons_clicked = []
         st.session_state.full_world_transposed[c][r] = '🎁'
-    else:
-        st.session_state.buttons_clicked = [(r,c)]
-        st.session_state.full_world_transposed[c][r] = [[row[i] for row in full_world] for i in range(len(full_world[0]))]
+    # else:
+    #     st.session_state.buttons_clicked = [(r,c)]
+    #     st.session_state.full_world_transposed[c][r] = [[row[i] for row in full_world] for i in range(len(full_world[0]))]
     return
 
 cols = st.columns(len(st.session_state.full_world_transposed[0]),gap='small')
@@ -33,5 +33,5 @@ cols = st.columns(len(st.session_state.full_world_transposed[0]),gap='small')
 for c,col in enumerate(st.session_state.full_world_transposed):
     with cols[c]:
         for r,element in enumerate(col):
-            if st.button(st.session_state.full_world_transposed[c][r], key = next(id_nums)): #, on_click=click_button, args=(button_key))
+            if st.button(st.session_state.full_world_transposed[c][r], key = next(id_nums)):
                 click_button(r,c)
