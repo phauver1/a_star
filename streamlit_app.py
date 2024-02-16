92dde3decf7764b2a34eed1ac807ed8b76136aa4
Desk1 <desk1@pop-os.localdomain>
2024-02-15 20:34:21 -0600

    S

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ if 'buttons_clicked' not in st.session_state:
 if 'full_world_transposed' not in st.session_state:
     st.session_state.full_world_transposed = [[row[i] for row in full_world] for i in range(len(full_world[0]))]
 
-st.write("Usage: Click the start position twice, then the destination twice, pausing for the map to reload before each click.")
+st.write("Usage: Click the start position twice, then the destination twice, pausing for the map to reload between each click.")
 
 cols = st.columns(len(st.session_state.full_world_transposed[0]),gap='small')
 def reset_buttons():
