cdd20c9fdfacd63093ecb81686e3b6ebe8d3a55b
Desk1 <desk1@pop-os.localdomain>
2024-02-15 19:59:56 -0600

    S

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,13 +22,11 @@ def click_button(r,c):
         st.session_state.buttons_clicked.append((r,c))
         st.session_state.full_world_transposed = [[row[i] for row in full_world] for i in range(len(full_world[0]))]
     elif len(st.session_state.buttons_clicked) == 1:
-        st.session_state.full_world_transposed[c][r] = 'X'
-        st.session_state.full_world_transposed[r][c] = 'Y'
         # for move in a_star_search(full_world, st.session_state.buttons_clicked[0], (r,c), COSTS, MOVES, heuristic):
         #     r += move[0]
         #     c += move[1]
         #     # st.session_state.full_world_transposed[c][r] = 'X'
-        # st.session_state.full_world_transposed[c][r] = '🎁'
+        st.session_state.full_world_transposed[c][r] = '🎁'
         st.session_state.buttons_clicked = []
     return
 
