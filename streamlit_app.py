222a4c27d5faa596cb3b0c30415684707f115043
Desk1 <desk1@pop-os.localdomain>
2024-02-15 19:34:57 -0600

    S

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,19 +14,20 @@ widget_id = (id for id in range(1, 100_00))
 
 if 'buttons_clicked' not in st.session_state:
     st.session_state.buttons_clicked = []
-print('full_world_transposed' in st.session_state)
-print('full_world_transposed' not in st.session_state)
 if 'full_world_transposed' not in st.session_state:
     st.session_state.full_world_transposed = [[row[i] for row in full_world] for i in range(len(full_world[0]))]
 
 cols = st.columns(len(st.session_state.full_world_transposed[0]),gap='small')
 
 def click_button(button_key):
-    if len(st.session_state.buttons_clicked) <= 1:
+    if len(st.session_state.buttons_clicked) == 0:
         st.session_state.buttons_clicked.append(button_key)
+    if len(st.session_state.buttons_clicked) == 1:
+        st.session_state.buttons_clicked.append(button_key)
+        st.session_state.full_world_transposed[c][r] = '🎁'
     else:
         st.session_state.buttons_clicked = [button_key]
-        st.full_world_transposed[c][r] = '🎁'
+        st.session_state.full_world_transposed[c][r] = [[row[i] for row in full_world] for i in range(len(full_world[0]))]
 
 # help(st.button)
 
