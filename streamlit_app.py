ffea712582716262e5d80c3bc9c17071cd7bf06a
Desk1 <desk1@pop-os.localdomain>
2024-02-16 11:09:27 -0600

    S

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,26 +18,28 @@ if 'full_world_transposed' not in st.session_state:
     st.session_state.full_world_transposed = [[row[i] for row in full_world] for i in range(len(full_world[0]))]
 if 'col_list' not in st.session_state:
     st.session_state.col_list = st.columns(len(st.session_state.full_world_transposed[1]),gap='small')
+if 'path_length' not in st.session_state:
+    st.session_state.path_length = 0
 
 with st.sidebar:
     st.title('A* Algorithm')
     st.write("Usage: Click the start position, pause for the map to reload, then click the destination.")
-    st.write("🌋 tiles are impassible, so clicks in those will be ignored")
+    st.write("Path Length: "+str(st.session_state.path_length))
 
 col_list = st.columns(len(st.session_state.full_world_transposed[0]),gap='small')
 
 def reset_buttons():
-    # cols = st.columns(len(st.session_state.full_world_transposed[0]),gap='small')
-    for c,col in enumerate(st.session_state.full_world_transposed):
-        with col_list[c]:
-            for r,element in enumerate(col):
-                st.button(st.session_state.full_world_transposed[c][r], key = next(id_nums), on_click=click_button, args=(r,c))
-    return
+    with st.empty():
+        for c,col in enumerate(st.session_state.full_world_transposed):
+            with col_list[c]:
+                for r,element in enumerate(col):
+                    st.button(st.session_state.full_world_transposed[c][r], key = next(id_nums), on_click=click_button, args=(r,c))
+        return
 
 def click_button(r,c):
     if st.session_state.full_world_transposed[c][r] == '🌋':
-        return
-    if len(st.session_state.buttons_clicked) == 0:
+        st.warning('🌋 tiles are impassible, so that click was ignored')
+    elif len(st.session_state.buttons_clicked) == 0:
         st.session_state.buttons_clicked.append((r,c))
         st.session_state.full_world_transposed = [[row[i] for row in full_world] for i in range(len(full_world[0]))]
         st.session_state.full_world_transposed[c][r] = '⭐'
@@ -45,9 +47,11 @@ def click_button(r,c):
         # st.session_state.buttons_clicked.append((r,c))
     elif len(st.session_state.buttons_clicked) == 1:
         ro,co = st.session_state.buttons_clicked[0]
+        st.session_state.path_length = 0
         for move in a_star_search(full_world, (co,ro), (c,r), COSTS, MOVES, heuristic):
             ro += move[1]
             co += move[0]
+            st.session_state.path_length += COSTS[st.session_state.full_world_transposed[co][ro]]
             st.session_state.full_world_transposed[co][ro] = MOVE_ICONS[MOVES.index(move)]
         st.session_state.full_world_transposed[c][r] = '🎁'
         st.session_state.buttons_clicked = []
